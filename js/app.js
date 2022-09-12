8269bb41e569811d0ff0fd870ab5ecc07f27fbb0
GUIDOlp <eliggiguido@hotmail.com>
2022-09-12 15:47:25 -0300

    new commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 
 
 
-const cargarContenido= async () => {
-    await fetch('js/productos.json')
-   .then((response) => response.json)
+function cargarContenido() {
+    fetch('js/productos.json')
+   .then((response) => response.json())
    .then((data) => console.table(data));
 }
 
